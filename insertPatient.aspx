04036ecf8f75da74e8c83d4493fefedb14ad28be
Bhakti Patel <115588802+Bhakti3001@users.noreply.github.com>
2023-07-25 13:06:42 -0400

    Add files via upload

---
+++ insertPatient.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="insertPatient.aspx.cs" Inherits="Morgue_Tracker.insertPatient" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <main>
+        <br />
+        <h1 class=" text-center" style=" font-size: 30px">Insert Patient</h1>
+        <br />
+        <br />
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 px-2 d-flex flex-column align-items-center">
+                    <div class="form-group">
+                        <asp:Label ID="lblPatientName" runat="server" Text="Patient Name" CssClass="left-align-label"></asp:Label>
+                        <asp:TextBox ID="txtPatientName" runat="server" class="form-control responsive-textbox"></asp:TextBox>
+                    </div>
+                    <br />
+                    <div class="form-group">
+                        <asp:Label ID="lblPatientID" runat="server" Text="Patient ID" CssClass="left-align-label"></asp:Label>
+                        <asp:TextBox ID="txtPatientID" runat="server" class="form-control responsive-textbox"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="col-md-6 px-2 d-flex flex-column align-items-center">
+                    <div class="form-group">
+                        <asp:Label ID="lblEmpName" runat="server" Text="Employee Name" CssClass="left-align-label"></asp:Label>
+                        <asp:TextBox ID="txtEmpName" runat="server" class="form-control responsive-textbox"></asp:TextBox>
+                    </div>
+                    <br />
+                    <div class="form-group">
+                        <asp:Label ID="lblEmpID" runat="server" Text="Employee ID" CssClass="left-align-label"></asp:Label>
+                        <asp:TextBox ID="txtEmpID" runat="server" class="form-control responsive-textbox"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 px-2 d-flex flex-column align-items-center">
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Upload" OnClick="btnSubmit_Click" class="btn btn-custom" />
+        </div>
+    </main>
+</asp:Content>
---
+++ insertPatient.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Morgue_Tracker
+{
+    public partial class insertPatient : System.Web.UI.Page
+    {
+       private  SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MorgueTrackerConn"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Get the input values from the form controls
+            try
+            {
+                conn.Open();
+                string patientName = txtPatientName.Text;
+                string patientID = txtPatientID.Text;
+                string employeeName = txtEmpName.Text;
+                string employeeID = txtEmpID.Text;
+
+                string query = "INSERT INTO MorgueTrackerTest (Patient_Name, Patient_ID, In_Employee_Name, In_Employee_ID, Created_Date) " +
+                   "VALUES (@patientName, @patientID, @employeeName, @employeeID, GETDATE())";
+
+                // Create a SqlCommand object to execute the query
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@patientName", patientName);
+                    cmd.Parameters.AddWithValue("@patientID", patientID);
+                    cmd.Parameters.AddWithValue("@employeeName", employeeName);
+                    cmd.Parameters.AddWithValue("@employeeID", employeeID);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        string successMessage = "Patient successfully inserted!";
+                        ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('{successMessage}');", true);
+                        txtPatientName.Text = "";
+                        txtPatientID.Text = "";
+                        txtEmpName.Text = "";
+                        txtEmpID.Text = "";
+                    }
+                    conn.Close();
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2627) // 2627 is the error number for a unique constraint violation (duplicate key).
+                {
+                    string errorMessage = "Patient ID must be unique. A patient with the same ID already exists!";
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('{errorMessage}');", true);
+                }
+                else
+                {
+                    string error = ex.ToString();
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('{error}');", true);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('{error}');", true);
+            }
+        }
+    }
+}
