8a0abd0a45e95d508b42f24bcdc13250572fd875
Bhakti Patel <115588802+Bhakti3001@users.noreply.github.com>
2023-07-27 11:05:14 -0400

    Update List.aspx.cs

--- ASPX.CS Files/List.aspx.cs
+++ ASPX.CS Files/List.aspx.cs
@@ -1,11 +1,13 @@
-using Microsoft.Ajax.Utilities;
+
+
+// Newer Version
+
 using System;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Globalization;
 using System.IO;
-using System.Net.NetworkInformation;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -13,117 +15,48 @@ namespace MorgueTracker3
 {
     public partial class List : Page
     {
-        private SqlConnection conn;
         private string selectAll = "SELECT Patient_Name, Patient_ID, In_Employee_Name, In_Employee_ID, Created_Date, Out_Employee_Name, Out_Employee_ID, Location_In_Morgue, Funeral_Home, Funeral_Home_Employee, Picked_Up_Date ";
-
-
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
             if (!IsPostBack)
             {
-                conn = CreateConnection();
                 BindGridView();
-
             }
         }
-
-
 
         protected void SearchByDate_Click(object sender, EventArgs e)
         {
-            lblStatus.Visible = false;
-            string dtStartDate = txtStartDate.Text;
-            string dtEndDate = txtEndDate.Text;
-            string query = selectAll + "FROM MorgueTracker ";
-            // Check if the start date or end date is empty
-            if (!string.IsNullOrEmpty(dtStartDate) || !string.IsNullOrEmpty(dtEndDate))
-            {
-                // If only the start date is provided
-                if (!string.IsNullOrEmpty(dtStartDate) && string.IsNullOrEmpty(dtEndDate))
-                {
-                    query += "WHERE CAST(Created_Date AS DATE) >= @startDate ";
-                }
-                // If only the end date is provided
-                else if (string.IsNullOrEmpty(dtStartDate) && !string.IsNullOrEmpty(dtEndDate))
-                {
-                    query += "WHERE CAST(Created_Date AS DATE) <= @endDate ";
-                }
-                // If both the start date and the end date are provided
-                else
-                {
-                    query += "WHERE CAST(Created_Date AS DATE) BETWEEN @startDate AND @endDate ";
-                }
-                if (PickUpCheck.Checked == false)
-                {
-                    query += "AND Funeral_Home IS NULL ";
-                }
-                else
-                {
-                    query += "AND Funeral_Home IS NOT NULL ";
-                }
-            }
-            else if (PickUpCheck.Checked == false)
-            {
-                query += " WHERE Funeral_Home IS NULL ";
-            }
-            else
-            {
-                query += " WHERE Funeral_Home IS NOT NULL ";
-            }
-            query += "ORDER BY Created_Date DESC";
-            conn = CreateConnection();
-            using (SqlCommand cmd = new SqlCommand(query, conn))
-            {
-                if (!string.IsNullOrEmpty(dtStartDate))
-                {
-                    cmd.Parameters.AddWithValue("@startDate", dtStartDate);
-                }
-                if (!string.IsNullOrEmpty(dtEndDate))
-                {
-                    cmd.Parameters.AddWithValue("@endDate", dtEndDate);
-                }
-                conn.Open();
-                DataTable dt = new DataTable();
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    dt.Load(reader);
-                }
-                gvList.DataSource = dt;
-                gvList.DataBind();
-                conn.Close();
-
-                if (gvList.Rows.Count == 0)
-                {
-                    lblStatus.Visible = true;
-                    lblStatus.Text = "No Data Found";
-                    lblStatus.Attributes.Add("style", "border-color: red;");
-                }
-            }
+            BindGridView();
         }
-
-
 
         protected void gvList_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             gvList.PageIndex = e.NewPageIndex;
-
             BindGridView();
         }
 
-
         private void BindGridView()
         {
-            
-            int startRow = gvList.PageIndex * gvList.PageSize;
-            string query = "SELECT * FROM MorgueTracker WHERE Funeral_Home IS NULL ORDER BY Created_Date DESC";
+            bool isPickedUp = PickUpCheck.Checked;
+            string dtStartDate = txtStartDate.Text;
+            string dtEndDate = txtEndDate.Text;
+
+            string query = BuildQuery(isPickedUp, dtStartDate, dtEndDate);
             string connectionString = ConfigurationManager.ConnectionStrings["MorgueTrackerConn"].ConnectionString;
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
+                    if (!string.IsNullOrEmpty(dtStartDate))
+                    {
+                        cmd.Parameters.AddWithValue("@startDate", dtStartDate);
+                    }
+                    if (!string.IsNullOrEmpty(dtEndDate))
+                    {
+                        cmd.Parameters.AddWithValue("@endDate", dtEndDate);
+                    }
 
                     conn.Open();
 
@@ -132,25 +65,61 @@ namespace MorgueTracker3
                     {
                         dt.Load(reader);
                     }
-                    conn.Close();
 
                     gvList.DataSource = dt;
                     gvList.DataBind();
+
+                    lblStatus.Visible = dt.Rows.Count == 0;
+                    if (dt.Rows.Count == 0)
+                    {
+                        lblStatus.Text = "No Data Found";
+                        lblStatus.Attributes.Add("style", "border-color: red;");
+                    }
                 }
             }
         }
 
-
-        private SqlConnection CreateConnection()
+        private string BuildQuery(bool isPickedUp, string startDate, string endDate)
         {
-            return new SqlConnection(ConfigurationManager.ConnectionStrings["MorgueTrackerConn"].ConnectionString);
+            string query = selectAll + "FROM MorgueTracker ";
+
+            if (!string.IsNullOrEmpty(startDate) || !string.IsNullOrEmpty(endDate))
+            {
+                if (!string.IsNullOrEmpty(startDate) && string.IsNullOrEmpty(endDate))
+                {
+                    query += "WHERE CAST(Created_Date AS DATE) >= @startDate ";
+                }
+                else if (string.IsNullOrEmpty(startDate) && !string.IsNullOrEmpty(endDate))
+                {
+                    query += "WHERE CAST(Created_Date AS DATE) <= @endDate ";
+                }
+                else
+                {
+                    query += "WHERE CAST(Created_Date AS DATE) BETWEEN @startDate AND @endDate ";
+                }
+
+                query += isPickedUp ? "AND Funeral_Home IS NOT NULL " : "AND Funeral_Home IS NULL ";
+            }
+            else
+            {
+                query += isPickedUp ? "WHERE Funeral_Home IS NOT NULL " : "WHERE Funeral_Home IS NULL ";
+            }
+
+            query += "ORDER BY Created_Date DESC";
+
+            return query;
         }
-
-
 
         protected void btnExport_Click(object sender, EventArgs e)
         {
+            int pageSize = gvList.PageSize;
+            int pageIndex = gvList.PageIndex;
+
+            gvList.AllowPaging = false;
+            BindGridView();
+
             string dateTimeWithHyphens = DateTime.Now.ToString("MM-dd-yyyy");
+
             if (gvList.Rows.Count == 0)
             {
                 lblStatus.Text = "Cannot export empty dataset.";
@@ -158,85 +127,87 @@ namespace MorgueTracker3
             }
             else
             {
-                DateTime time = DateTime.Now;
-                Response.Clear();
-                Response.Buffer = true;
-                String pickedUp = "";
-                if (PickUpCheck.Checked == false)
-                {
-                    pickedUp = "Not_Picked_Up";
-                }
-                else
-                {
-                    pickedUp = "Picked_Up";
-                }
-                if (txtStartDate.Text.IsNullOrWhiteSpace() && txtEndDate.Text.IsNullOrWhiteSpace())
-                {
+                ExportGridToExcel(dateTimeWithHyphens);
+            }
 
-                    Response.AddHeader("content-disposition", "attachment;filename=" + pickedUp + "MorguePatients_as_of(" + dateTimeWithHyphens + ").xls");
-                }
-                else if (txtEndDate.Text.IsNullOrWhiteSpace())
-                {
-                    // Original date string in "yyyy-mm-dd" format
-                    string startOriginalDateString = txtStartDate.Text;
+            gvList.AllowPaging = true;
+            gvList.PageSize = pageSize;
+            gvList.PageIndex = pageIndex;
+            BindGridView();
+        }
 
-                    // Parse the original date string into a DateTime object
-                    DateTime startDate = DateTime.ParseExact(startOriginalDateString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        private void ExportGridToExcel(string dateTimeWithHyphens)
+        {
+            string pickedUp = PickUpCheck.Checked ? "Picked_Up" : "Not_Picked_Up";
+            string fileName = GetExportFileName(pickedUp, dateTimeWithHyphens);
 
-                    // Format the DateTime object into the "MM-dd-yyyy" format
-                    string startFormattedDateString = startDate.ToString("MM-dd-yyyy");
-                    Response.AddHeader("content-disposition", "attachment;filename=" + pickedUp + "_MorguePatients(" + startFormattedDateString + "_to_" + dateTimeWithHyphens + ").xls");
-                }
-                else if (txtStartDate.Text.IsNullOrWhiteSpace())
-                {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=" + fileName);
+            Response.Charset = "";
+            Response.ContentType = "application/vnd.ms-excel";
 
-                    string endOriginalDateString = txtEndDate.Text;
-                    DateTime endDate = DateTime.ParseExact(endOriginalDateString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                    string endFormattedDateString = endDate.ToString("MM-dd-yyyy");
-                    Response.AddHeader("content-disposition", "attachment;filename=" + pickedUp + "_MorguePatients_until(" + endFormattedDateString + ").xls");
-                }
-                else
-                {
-                    string startOriginalDateString = txtStartDate.Text;
-                    DateTime startDate = DateTime.ParseExact(startOriginalDateString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                    string startFormattedDateString = startDate.ToString("MM-dd-yyyy");
-                    string endOriginalDateString = txtEndDate.Text;
-                    DateTime endDate = DateTime.ParseExact(endOriginalDateString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                    string endFormattedDateString = endDate.ToString("MM-dd-yyyy");
-                    Response.AddHeader("content-disposition", "attachment;filename=" + pickedUp + "_MorguePatients(" + startFormattedDateString + "_to_" + endFormattedDateString + ").xls");
-                }
-                Response.Charset = "";
-                Response.ContentType = "application/vnd.ms-excel";
-                using (StringWriter sw = new StringWriter())
-                {
-                    HtmlTextWriter hw = new HtmlTextWriter(sw);
-                    // Change the Header Row back to white color
-                    gvList.HeaderRow.Style.Add("background-color", "#FFFFFF");
-                    // Apply styles to each Header cell
-                    foreach (TableCell cell in gvList.HeaderRow.Cells)
-                    {
-                        cell.Attributes.Add("background-color", "#edfbfb;");
-                    }
-                    foreach (GridViewRow row in gvList.Rows)
-                    {
-                        // Apply styles to each row
-                        row.BackColor = System.Drawing.Color.White;
-                        foreach (TableCell cell in row.Cells)
-                        {
-                            cell.CssClass = "textmode";
-                            cell.Attributes.Add("style", "mso-number-format:\\@");
-                        }
-                    }
-                    gvList.RenderControl(hw);
-
-                    // Write the output to the response
-                    Response.Output.Write(sw.ToString());
-                    Response.Flush();
-                    Response.End();
-                }
+            using (StringWriter sw = new StringWriter())
+            {
+                HtmlTextWriter hw = new HtmlTextWriter(sw);
+                StyleGridForExport(hw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
             }
         }
 
+        private string GetExportFileName(string pickedUp, string dateTimeWithHyphens)
+        {
+            string startDateText = txtStartDate.Text;
+            string endDateText = txtEndDate.Text;
+
+            if (string.IsNullOrEmpty(startDateText) && string.IsNullOrEmpty(endDateText))
+            {
+                return $"{pickedUp}_Morgue_Patients_as_of({dateTimeWithHyphens}).xls";
+            }
+            else if (string.IsNullOrEmpty(endDateText))
+            {
+                DateTime startDate = DateTime.ParseExact(startDateText, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                string startFormattedDateString = startDate.ToString("MM-dd-yyyy");
+
+                return $"{pickedUp}_Morgue_Patients({startFormattedDateString}_to_{dateTimeWithHyphens}).xls";
+            }
+            else if (string.IsNullOrEmpty(startDateText))
+            {
+                DateTime endDate = DateTime.ParseExact(endDateText, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                string endFormattedDateString = endDate.ToString("MM-dd-yyyy");
+
+                return $"{pickedUp}_Morgue_Patients_until({endFormattedDateString}).xls";
+            }
+            else
+            {
+                DateTime startDate = DateTime.ParseExact(startDateText, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                string startFormattedDateString = startDate.ToString("MM-dd-yyyy");
+                DateTime endDate = DateTime.ParseExact(endDateText, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                string endFormattedDateString = endDate.ToString("MM-dd-yyyy");
+
+                return $"{pickedUp}_Morgue_Patients({startFormattedDateString}_to_{endFormattedDateString}).xls";
+            }
+        }
+
+        private void StyleGridForExport(HtmlTextWriter hw)
+        {
+            gvList.HeaderRow.Style.Add("background-color", "#edfbfb");
+
+            foreach (GridViewRow row in gvList.Rows)
+            {
+                row.BackColor = System.Drawing.Color.White;
+
+                foreach (TableCell cell in row.Cells)
+                {
+                    cell.CssClass = "textmode";
+                    cell.Attributes.Add("style", "mso-number-format:\\@");
+                }
+            }
+
+            gvList.RenderControl(hw);
+        }
 
 
         public override void VerifyRenderingInServerForm(Control control)
@@ -246,3 +217,5 @@ namespace MorgueTracker3
         }
     }
 }
+
+
